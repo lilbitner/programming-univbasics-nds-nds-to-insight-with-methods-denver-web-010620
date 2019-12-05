e6fd7d487748d97ea3a433462d0c74c6e6848d4a
Lillian Bitnr <lillian.bitner@gmail.com>
2019-12-05 19:14:27 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -13,5 +13,13 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  coordinate_total = log10
+  coordinate_total = 0
+  inner_len = nds[row_index][column_index].length
+  inner_index = 0
+  while inner_index < inner_len do
+    coordinate_total += nds[row_index][column_index][inner_index][:total]
+    inner_index += 1
+  end
+  coordinate_total
 end
+
